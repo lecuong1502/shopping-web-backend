26f405fe95d628a04a9c386888e7e804d259d3a2
Thang Le <tleduc@pentalog.com>
2024-04-08 23:45:47 +0700

    api signup

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
+const { queryAsync } = require("./src/database");
 
 const app = express();
 app.use(bodyParser.json());
 
+app.post("/api/signup", async (req, res) => {
+  try {
+    const { name, password, gmail, phoneNum, address } = req.body;
+    // TODO: hash password
+    const signUpQuery = `INSERT INTO User (name, password, gmail, phoneNum, address) VALUES ('${name}', '${password}', '${gmail}', '${phoneNum}', '${address}');`;
+    await queryAsync(signUpQuery);
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ error: "An error occur" });
+  }
+});
 
 app.get("/api/login", (req, res) => {
-    res.json({ data: 123 })
+  // TODO: check login
+  res.json({ data: 123 });
 });
 
 // Start the server
---
+++ src/database.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");
+const util = require("util");
+
+const dbParams = {
+  host: "127.0.0.1",
+  user: "root",
+  password: "admin",
+  database: "shoppingWeb",
+};
+
+const db = mysql.createConnection(dbParams);
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
+const queryAsync = util.promisify(db.query).bind(db);
+
+module.exports = {
+  db,
+  queryAsync,
+};
